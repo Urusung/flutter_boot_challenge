cd081338995b8c2da829fd3acf3d3b0808f395fc
최우성 <choiwoosung61@gmail.com>
2024-01-24 17:42:55 +0900

    fix

---
+++ lib/screens/hello_layout_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HelloLayoutScreen extends StatelessWidget {
+  const HelloLayoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/my_high_score_screen.dart
+++ lib/screens/my_high_score_screen.dart
@@ -1,33 +1,29 @@
 import 'dart:async';
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyHighScoreScreen extends StatefulWidget {
-  const MyHighScoreScreen({super.key});
+  const MyHighScoreScreen({Key? key}) : super(key: key);
 
   @override
   State<MyHighScoreScreen> createState() => _MyHighScoreScreenState();
 }
 
 class _MyHighScoreScreenState extends State<MyHighScoreScreen> {
-  int yourScore = 0;
-  double containerHeight = 0;
+  int score = 0;
+  double gaugeHeight = 0;
+  final double gaugeMaxHeight = 420;
+  late Timer? decreaseTimer;
 
   @override
   void initState() {
     super.initState();
-    Timer.periodic(const Duration(milliseconds: 100), (timer) {
-      setState(() {
-        print('Container Height: $containerHeight');
-        containerHeight -= 100;
-        if (containerHeight <= 0) {
-          containerHeight = 0;
-          yourScore = 0;
-        }
-      });
-    });
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    stopTimer();
+    super.dispose();
   }
 
   @override
@@ -35,75 +31,107 @@ class _MyHighScoreScreenState extends State<MyHighScoreScreen> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding:
-              const EdgeInsets.only(top: 24, right: 16, left: 16, bottom: 16),
+          padding: const EdgeInsets.all(16),
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text(
-                  'Your score',
-                  style: TextStyle(fontSize: 32),
-                ),
-                const SizedBox(height: 16),
+                const Text('Your score'),
                 Text(
-                  '$yourScore',
-                  style: const TextStyle(
-                      fontSize: 42, fontWeight: FontWeight.bold),
+                  '$score',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const Spacer(),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: gaugeMaxHeight,
+                            decoration: BoxDecoration(
+                              color: Colors.deepPurple.shade100,
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(24),
+                                topRight: Radius.circular(24),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 50,
+                            height: gaugeHeight,
+                            decoration: const BoxDecoration(
+                              color: Colors.deepPurple,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(24),
+                                topRight: Radius.circular(24),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: FloatingActionButton(
+                          onPressed: increase,
+                          child: const Text(
+                            '+',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
         ),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.bottomCenter,
-            children: [
-              Container(
-                width: 40,
-                height: 420,
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16),
-                    topRight: Radius.circular(16),
-                  ),
-                ),
-              ),
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 100),
-                width: 40,
-                height: containerHeight,
-                decoration: const BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16),
-                    topRight: Radius.circular(16),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                containerHeight = containerHeight + 140;
-                print('Container Height: $containerHeight');
-                if (containerHeight >= 420) {
-                  containerHeight = 420;
-                  yourScore = yourScore += 1;
-                }
-              });
-            },
-            child: const Icon(Icons.add),
-          ),
-        ],
-      ),
     );
+  }
+
+  startTimer() {
+    decreaseTimer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        gaugeHeight -= 4.2;
+      });
+
+      if (gaugeHeight <= 0) {
+        gaugeHeight = 0;
+
+        if (score != 0) {
+          setState(() {
+            score = 0;
+          });
+        }
+      }
+    });
+  }
+
+  stopTimer() {
+    decreaseTimer?.cancel();
+  }
+
+  increase() {
+    setState(() {
+      gaugeHeight += 420 / 3;
+    });
+
+    if (gaugeHeight >= gaugeMaxHeight) {
+      gaugeHeight = gaugeMaxHeight;
+
+      setState(() {
+        score++;
+      });
+    }
   }
 }
